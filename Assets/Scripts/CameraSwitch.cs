f9b5be1666e003e33dac962500402aaaaa2f93c4
Louise Collins <louisecollins1971@icloud.com>
2025-06-28 23:45:43 +0100

    Sorted out a better Switch Camera System

---
+++ Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    [SerializeField] private GameObject cinemachineCamera;
+    [SerializeField] private bool revertOnExit = true; // Default to false
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cinemachineCamera.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Do nothing to keep the camera active afterward
+        if (other.CompareTag("Player") && revertOnExit)
+        {
+            cinemachineCamera.SetActive(true);
+        }
+    }
+}
