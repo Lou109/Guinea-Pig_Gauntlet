78ba220b747611681306012fdbeb917d373f48ba
Louise Collins <louisecollins1971@icloud.com>
2025-07-01 11:42:50 +0100

    backup

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -2,25 +2,39 @@ using UnityEngine;
 
 public class CameraSwitch : MonoBehaviour
 {
-    [SerializeField] private GameObject cinemachineCamera;
-    [SerializeField] private bool revertOnExit = true; // Default to false
+    [SerializeField] private GameObject[] cameras; // Assign all your cameras here in the inspector
+    [SerializeField] private int cameraIndex; // This determines which camera this trigger zone activates
+    [SerializeField] private bool revertOnExit = true;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            cinemachineCamera.SetActive(false);
+            SwitchCamera(cameraIndex);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        // Do nothing to keep the camera active afterward
         if (other.CompareTag("Player") && revertOnExit)
         {
-            cinemachineCamera.SetActive(true);
+            SwitchCamera(cameraIndex - 1); // Or however, you determine the previous camera
         }
     }
-} 
+
+    private void SwitchCamera(int index)
+    {
+        if (index < 0 || index >= cameras.Length) return;
+
+        // Deactivate all cameras
+        foreach (var cam in cameras)
+        {
+            cam.SetActive(false);
+        }
+
+        // Activate the desired camera
+        cameras[index].SetActive(true);
+    }
+}
 
     
